46628983ee03473fba6ffc4e249a6a7920240488
Bah Mamadou Cellou Kesso <cellouceleste2013@gmail.com>
2023-01-10 21:32:16 +0000

    gestion des notifications

--- js/script.js
+++ js/script.js
@@ -4,15 +4,18 @@ const mauvaiseLettres = document.querySelector('.lettreErronees');
 const btnVerifier = document.getElementById('verifier');
 const dessin = document.querySelectorAll('.dessin-bonhomme');
 const notification = document.querySelector('.gagne');
+const zoneSaisie =document.getElementById('zoneSaisie');
 const btnRejouer = document.querySelector('#btn-rejouer');
 const motADeviner = ['guinee','devellopement','believemy','programmation','javascript','france'];
-const tableauDeBonnesLettres = [];
-const tableauDeMauvaisesLettres = [];
+
 
 // Selectionner un mot dans le tableau
 
 let motSelectionne = motADeviner[Math.floor(Math.random() * motADeviner.length)];
 console.log(motSelectionne);
+
+const tableauDeBonnesLettres = [];
+const tableauDeMauvaisesLettres = [];
 // Afficher le mot
 
 function afficherMot() {
@@ -29,79 +32,92 @@ function afficherMot() {
     console.log(affichageBonnesLettres.innerText, motAfficher);
 
     //Verifier si le mot afficher correspond au mot selectionné
-    if(motAfficher == motSelectionne){
+    if(motAfficher === motSelectionne){
         notification.innerText = 'Bravo, vous avez gagné !';
-        notification.style.transition = 'transfor 0.3 ease-in-out';
+        notification.style.display = "block";
+        notification.style.transition = 'all 0.3 ease-in-out';
+        btnRejouer.style.display = "block";
+        zoneSaisie.setAttribute('disabled',true);
     }else{
         notification.style.display='none';
-    }
+    } 
+}
 
-    // Afficher les mauvaises lettres
-    function miseAJourMauvaisesLettres(){
-        // Afficher les mauvaise lettres
-        mauvaiseLettres.innerHTML = `
-            ${tableauDeMauvaisesLettres.map(lettre => `<span>${lettre}</span>`)}
-        `;
+// Afficher les mauvaises lettres
+function miseAJourMauvaisesLettres(){
+    // Afficher les mauvaise lettres
+    mauvaiseLettres.innerHTML = `
+        ${tableauDeMauvaisesLettres.map(lettre => `<span>${lettre}</span>`)}
+    `;
+   
+    // Afficher le corps
+    dessin.forEach((partieDuCorps, index) => {
+        const nbErreurs = tableauDeMauvaisesLettres.length;
 
-        // Afficher le corps
-        dessin.forEach((partieDuCorps, index) => {
-            const nbErreurs = tableauDeMauvaisesLettres.length;
-
-            if(index < nbErreurs){
-                partieDuCorps.style.display = "block";
-            }else{
-                partieDuCorps.style.display = "none";
-            }
-        });
-
-        // Vérifier si on a perdu
-        if(tableauDeMauvaisesLettres.length == partieDuCorps.length){
-            document.querySelector('.gagne-phase').innerHTML = "Désolé, vous avez perdu !";
-            notification.style.display = "block";
-            btnRejouer.style.display = "block";
-        }
-    }
-
-    // Ajouter des évènements
-    window.addEventListener('keyup', e => {
-        if(e.keyCode >= 65 && e.keyCode <= 90) {
-            const laLettre = e.key;
-
-            if(motSelectionne.includes(laLettre)) {
-
-                if(!tableauDeBonnesLettres.includes(laLettre)){
-
-                    tableauDeBonnesLettres.push(laLettre);
-                    afficherMot();
-                }else{
-                    
-                    notification.style.display="block";
-                    notification.innerText = 'Vous avez déjà essayé cette lettre !';
-                    setTimeout(() => {
-                        notification.style.display = 'none';
-                    }, 2000);
-                }
-            }else{
-
-                if(!tableauDeMauvaisesLettres.includes(laLettre)){
-                    tableauDeMauvaisesLettres.push(laLettre);
-
-                    miseAJourMauvaisesLettres();
-                }else{
-                    //affichierNotification();
-                }
-            }
+        if(index < nbErreurs){
+            partieDuCorps.style.display = "block";
+        }else{
+            partieDuCorps.style.display = "none";
         }
     });
+
+    // Vérifier si on a perdu
+    if(tableauDeMauvaisesLettres.length === dessin.length){
+        document.querySelector('.gagne').innerText = "Désolé, vous avez perdu !";
+        document.querySelector('.gagne').style.display = "block";
+        btnRejouer.style.display = "block";
+        zoneSaisie.setAttribute('disabled',true);
+    }
 }
+
+// Ajouter des évènements
+window.addEventListener('keydown', e => {
+    if(e.keyCode >= 65 && e.keyCode <= 90) {
+        const laLettre = e.key;
+
+        if(motSelectionne.includes(laLettre)) {
+
+            if( !tableauDeBonnesLettres.includes(laLettre)){
+
+                tableauDeBonnesLettres.push(laLettre);
+                afficherMot();
+            }else{
+
+                notification.style.display="block";
+                notification.innerText = 'Vous avez déjà essayé cette lettre !';
+                setTimeout(() => {
+                    notification.style.display = 'none';
+                }, 5000);
+            }
+        }else{
+
+            if(!tableauDeMauvaisesLettres.includes(laLettre)){
+                tableauDeMauvaisesLettres.push(laLettre);
+
+                miseAJourMauvaisesLettres();
+            }else{
+                
+                //affichierNotification();
+                
+            }
+        }
+    }
+});
 
 // Redémarrer le jeu
 btnRejouer.addEventListener('click', () => {
-    tableauDeBonnesLettres.slice(0);
-    tableauDeMauvaisesLettres.slice(0);
+    tableauDeBonnesLettres.splice(0);
+    tableauDeMauvaisesLettres.splice(0);
     motSelectionne = motADeviner[Math.floor(Math.random() * motADeviner.length)];
+    zoneSaisie.value = "";
+    zoneSaisie.removeAttribute('disabled');
+    zoneSaisie.focus();
+    mauvaiseLettres.innerText = "";
+    btnRejouer.style.display = "none";
+    dessin.forEach(el => el.style.display = "none");
     afficherMot();
-    miseAJourMauvaisesLettres();
-    notification.style.display = 'none';
+    //miseAJourMauvaisesLettres();
+    document.querySelector('.gagne').style.display = 'none';
+    console.log(motSelectionne);
 });
 afficherMot();
